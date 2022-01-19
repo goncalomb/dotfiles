2a404b33ad8a8d25157485cc585cfc566051e9ce
Gonçalo Baltazar <me@goncalomb.com>
2022-01-19 21:20:20 +0000

    Add install for git-mtime

--- install-gists.sh
+++ install-gists.sh
@@ -27,3 +27,4 @@ dl_gist() {
 
 dl_gist "goncalomb/6d879df103fda9b63feb/raw/img2ico.php" "img2ico" "#!/bin/env php"
 dl_gist "goncalomb/13f28e459fe4dd656e8b43f92c826140/raw/git-problems"
+dl_gist "goncalomb/a71fd8581c330e4e2f86a3c15af1e3f7/raw/git-mtime"
